41f4586a787719c51a892ce1b0443bb2711f5f7b
vir dhoriya <virdhoriya@gmail.com>
2024-12-24 16:07:34 +0530

    Implement media queries for mobile responsiveness

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="section-footer">
-      <div className="container">
+      <div className="secondary-container">
         <div className="flex justify-center items-center">
           <img
             src="/logo.png"
@@ -9,13 +9,23 @@ const Footer = () => {
             className="footer-logo"
           />
         </div>
-        <div className="pt-24 flex justify-center items-center">
+        <div className="pt-24 flex justify-center items-cente md-s:pt-20 tb-l:hidden">
           <ul className="sitemap">
-            <li><a href="#">Home</a></li>
-            <li><a href="#about">Discover Us</a></li>
-            <li><a href="#choose-us">Why Us ?</a></li>
-            <li><a href="#services">Our Services</a></li>
-            <li><a href="#contact">Get In Touch</a></li>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#about">Discover Us</a>
+            </li>
+            <li>
+              <a href="#choose-us">Why Us ?</a>
+            </li>
+            <li>
+              <a href="#services">Our Services</a>
+            </li>
+            <li>
+              <a href="#contact">Get In Touch</a>
+            </li>
           </ul>
         </div>
         <p className="copyright-text">
